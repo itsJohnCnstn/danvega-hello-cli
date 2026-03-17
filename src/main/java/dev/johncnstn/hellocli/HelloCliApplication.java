0efe51ebbd2b65786b4aaef156b5f475d14a7198
Pavel Khankevich <johncnstn@gmail.com>
2026-03-17 19:27:21 +0100

    init

---
+++ src/main/java/dev/johncnstn/hellocli/HelloCliApplication.java
@@ -0,0 +1,13 @@
+package dev.johncnstn.hellocli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloCliApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloCliApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/johncnstn/hellocli/HelloCliApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.johncnstn.hellocli;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloCliApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
